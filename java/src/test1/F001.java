fd2a089e0b32f58856cf91e9986c10976b573ad0
kangning <3390142252@qq.com>
2022-04-16 18:26:13 +0800

    1

--- java/src/test1/F001.java
+++ java/src/test1/F001.java
@@ -2,9 +2,39 @@ package test1;
 
 public class F001 {
     public static void main(String[] args){
-        String a;
-        a = new String("asdfghj");
-        //Integer a=555;
-        System.out.println(a.toString());
+        String a="hello world";
+        System.out.println(a);
+        Parents parents=new Parents(88);
+        parents.print();
+        Son son=new Son(44);
+        son.print();
+        son.print1();
+
+    }
+}
+class Parents{
+    int age,high;
+    public Parents(int age){
+    this.age=age;
+    this.high=100;
+    }
+    public void print(){
+        System.out.println("I'm parents , my age is "+this.age);
+    }
+}
+class Son extends Parents{
+    int age,high;
+    public Son(int age){
+        super(age);
+        this.high=50;
+    }
+
+    public void print(){
+        System.out.println("I'm son , super.age = "+super.age);
+        System.out.println("this.son = "+this.age);
+        System.out.println("this.high = "+this.high);
+    }
+    public void print1(){
+        super.print();
     }
 }
---
+++ java/src/test1/F002.java
@@ -0,0 +1,66 @@
+package test1;
+
+ class A {
+    public String s = "123";
+
+    public void show(A obj){
+        System.out.println("A and A");
+    }
+
+    public void show(D obj){
+        System.out.println("A and D");
+    }
+}
+
+class B extends A{
+    public String s = "456";
+
+    public void show(A obj){
+        System.out.println("B and A");
+    }
+
+    public void show(B obj){
+        System.out.println("B and B");
+    }
+
+    public void show(E obj){
+        System.out.println("B and E");
+    }
+}
+
+class C extends B{
+}
+
+class D extends B{
+}
+
+class E{
+}
+public class F002 {
+    public static void main(String[] args){
+        A a1 = new A();
+        A a2 = new B();
+
+        B b = new B();
+        C c = new C();
+        D d = new D();
+        E e = new E();
+
+        a1.show(b);
+        a1.show(c);
+        a1.show(d);
+        System.out.println(a1.s);
+
+        a2.show(b);
+        a2.show(c);
+        a2.show(d);
+        //a2.show(e);//这句编译不过
+        System.out.println(a2.s);
+
+        b.show(b);
+        b.show(c);
+        b.show(d);
+        b.show(e);
+        System.out.println(b.s);
+    }
+}
