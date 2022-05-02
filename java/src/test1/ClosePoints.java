7a9896280bb629ed34b2a1160ae2820e4d9499ca
kangning <3390142252@qq.com>
2022-05-02 13:13:02 +0800

    1

--- java/src/test1/ClosePoints.java
+++ java/src/test1/ClosePoints.java
@@ -1,93 +1,111 @@
 package test1;
 
-import java.security.PublicKey;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Scanner;
 
-class Point implements Comparable{
+class Point {
     private double x;
     private double y;
-    public double getX(){
+
+    Point() {
+        x = 0.0;
+        y = 0.0;
+    }
+
+    public double getX() {
         return x;
     }
-    public double getY(){
+
+    public double getY() {
         return y;
     }
-    public void setX(double x){
-        this.x=x;
+
+    public void setX(double x) {
+        this.x = x;
     }
-    public void setY(double y){
-        this.y=y;
-    }
-    public int compareTo(Object a){
-        return (int)((this.getX()-((Point)a).getX()));
+
+    public void setY(double y) {
+        this.y = y;
     }
 }
-public class ClosePoints {
-    public static void main(String... args){
 
+public class ClosePoints {
+    public static void main(String... args) {
+        Scanner input=new Scanner(System.in);
+        int length=input.nextInt();
+        Point[] points=new Point[length];
+        for(int i=0;i<length;i++){
+            points[i]=new Point();
+            points[i].setX(input.nextDouble());
+            points[i].setY(input.nextDouble());
+        }
+//        int length = 4;
+//        Point[] points = new Point[length];
+//        for (int i = 0; i < length; i++) {
+//            points[i] = new Point();
+//        }
+//        points[0].setX(1.0);
+//        points[0].setY(1.0);
+//        points[1].setX(2.0);
+//        points[1].setY(2.0);
+//        points[2].setX(4.0);
+//        points[2].setY(4.0);
+//        points[3].setX(8.0);
+//        points[3].setY(8.0);
+        System.out.println("closest distance is "+search(points, length));
     }
-    public static double distance(Point a,Point b){
-        return Math.sqrt(Math.pow(a.getX()-b.getX(),2)+Math.pow(a.getX()-b.getX(),2));
+
+    public static double distance(Point a, Point b) {
+        return Math.sqrt(Math.pow(a.getX() - b.getX(), 2) + Math.pow(a.getY() - b.getY(), 2));
     }
-    public static double search(Point[] points,int length,Point a,Point b){
-        if(length<2)return 999999999;
-        if(length==2)return distance(points[0],points[1]);
-        Point[] points1=new Point[length];
-        Point[] points2=new Point[length];
-        Point[] points3=new Point[length];
-        Point a1=new Point(),a2=new Point(),b1=new Point(),b2=new Point();
-        double distance,d1,d2;
+    public static double search(Point[] points, int length) {
+        if (length < 2) return Double.POSITIVE_INFINITY;
+        if (length == 2) return distance(points[0], points[1]);
+        ArrayList<Point> points1 = new ArrayList<>();
+        ArrayList<Point> points2 = new ArrayList<>();
+        ArrayList<Point> points3 = new ArrayList<>();
+        double distance, d1, d2;
 
         Arrays.sort(points, new Comparator<Point>() {
             @Override
             public int compare(Point o1, Point o2) {
-                return (int)(o1.getX()-o2.getX());
+                return (int) (o1.getX() - o2.getX());
             }
         });
-        double mid=points[length/2].getX();
-        for(int i=0;i<length/2;i++){
-            points1[i]=points[i];
+        double mid = points[length / 2].getX();
+        for (int i = 0; i < length / 2; i++) {
+            points1.add(points[i]);
         }
-        for(int j=0,i=length/2;i<length;i++){
-            points2[j++]=points[i];
+        for (int j = 0, i = length / 2; i < length; i++) {
+            points2.add(points[j++]);
         }
-        d1=search(points1,length/2,a1,b1);
-        d2=search(points2,length-length/2,a2,b2);
-        if(d1<d2){
-            distance=d1;
-            a=a1;
-            b=b1;
-        }else{
-            distance=d2;
-            a=a2;
-            b=b2;
-        }
-        int i=0,j=0,k=0,x=0;
-        for(i=0,k=0;i<length;i++){
-            if(Math.abs(points[i].getX()-mid)<=distance){
-                points3[k++]=points[i];
+        d1 = search(points1.toArray(new Point[points1.size()]), length / 2);
+        d2 = search(points2.toArray(new Point[points2.size()]), length - length / 2);
+        distance=Math.min(d1,d2);
+        int i = 0, j = 0, k = 0, x = 0;
+        for (i = 0, k = 0; i < length; i++) {
+            if (Math.abs(points[i].getX() - mid) <= distance) {
+                points3.add(points[i]);
             }
         }
-        Arrays.sort(points3, new Comparator<Point>(){
+        Arrays.sort(points3.toArray(new Point[points3.size()]), new Comparator<Point>() {
             @Override
-            public int compare(Point o1,Point o2){
-                return (int)(o1.getY()-o2.getY());
+            public int compare(Point o1, Point o2) {
+                return (int) (o1.getY() - o2.getY());
             }
         });
-        for(i=0;i<k;i++){
-            if(points3[i].getY()>mid)continue;
-            x=0;
-            for(j=i+1;j<=i+6+6;j++){
-                if(points[j].getX()<=mid){
+        for (i = 0; i < k; i++) {
+            if (points3.get(i).getY() > mid) continue;
+            x = 0;
+            for (j = i + 1; j <= i + 6 + 6; j++) {
+                if (points[j].getX() <= mid) {
                     x++;//jump the left point
                     continue;
                 }
-                if(distance(points3[i],points3[j])<distance){
-                    distance=distance(points3[i],points3[j]);
-                    a=points3[i];
-                    b=points3[j];
+                if (distance(points3.get(i), points3.get(j)) < distance) {
+                    distance = distance(points3.get(i), points3.get(j));
                 }
             }
         }
