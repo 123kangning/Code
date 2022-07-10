1c9195a481a2aa0461fcdc976b59ebc2f46b1fc9
123kangning <3390142252@qq.com>
2022-07-10 23:08:04 +0800

    1

--- java/src/IO/NIOClient.java
+++ java/src/IO/NIOClient.java
@@ -1,18 +1,21 @@
 package IO;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.Socket;
 
 public class NIOClient {
 
-    public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("127.0.0.1", 8888);
-        InputStream out = socket.getInputStream();
-//        String s = "hello world";
-//        out.write(s.getBytes());
-        System.out.println(out.read());
-        out.close();
+    public static void main(String[] args) throws IOException,InterruptedException{
+        Socket socket = new Socket("127.0.0.1", 8881);
+        while(true){
+            InputStream out = socket.getInputStream();
+            Reader read=new BufferedReader(new InputStreamReader(out));
+            char[] a=new char[20];
+            read.read(a);
+            System.out.print(a);
+//            read.close();
+            Thread.sleep(1000);
+        }
     }
 }
 
--- java/src/IO/Serve1.java
+++ java/src/IO/Serve1.java
@@ -1,6 +1,7 @@
 package IO;
 
 import java.io.*;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
@@ -8,20 +9,38 @@ import java.util.Date;
 
 public class Serve1 {
     public static void main(String[] args) throws IOException {
-        ServerSocket server=new ServerSocket(8888);
-        while(true){
-            try(Socket connection=server.accept()){
-                System.out.println("run");
-                OutputStream out=connection.getOutputStream();
-                Writer writer=new BufferedWriter(new OutputStreamWriter(out, StandardCharsets.US_ASCII));
-                Date now=new Date();
-                writer.write(now+"\n");
-                out.flush();
-            }catch(IOException ex){
-                ex.printStackTrace();
-            }
-        }
+        try(ServerSocket server=new ServerSocket()){
+            server.setReuseAddress(true);
+            InetSocketAddress address=new InetSocketAddress(8881);
+            server.bind(address);
+            while(true){
 
-//        connection.close();
+                try(Socket connection=server.accept()){
+
+                    System.out.println("run");
+                    OutputStream out=connection.getOutputStream();
+                    Writer writer=new BufferedWriter(new OutputStreamWriter(out, StandardCharsets.US_ASCII));
+                    Date now=new Date();
+                    writer.write(now+"\n");
+                    writer.flush();
+                    long lastTime=now.getTime()/1000,nowTime;
+                    while(connection!=null){
+                        now=new Date();
+                        nowTime=now.getTime();
+                        if(nowTime%1000==0&&nowTime/1000!=lastTime){
+                            writer.write(now+"\n");
+                            writer.flush();
+                            lastTime=nowTime/1000;
+                        }
+                    }
+                    System.out.println(now+"\n");
+
+                }catch(IOException ex){
+                    ex.printStackTrace();
+                }
+            }
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }
     }
 }
