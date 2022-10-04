8408e6bc4d6eb01dadde77912092f4094d278044
123kangning <3390142252@qq.com>
2022-10-04 22:10:50 +0800

    1

--- c/test/test4.c
+++ c/test/test4.c
@@ -1,14 +1,13 @@
 //汉诺塔非递归
 #include <stdio.h>
 #include <assert.h>
-#include <stdarg.h>
 
 typedef struct {
     int pc,n;
     char from,to,via;
 } Frame;
 
-#define call(...) ({*(++top) = (Frame){.pc=0,__VA_ARGS__};})
+#define call(_n,_from,_to,_via) ({*(++top) = (Frame){.pc=0,.n=(_n),.from=(_from),.to=(_to),.via=(_via)};})
 #define ret() ({top--;})
 #define goto(loc) ({f->pc=(loc)-1;})
 
@@ -40,4 +39,7 @@ int main(void)
 考点1：宏函数
 考点2：汉诺塔思想
 思考点：函数调用与返回时栈帧的变化
+提示：可以理解为在一个递归函数中有
+case0 case1 case2 case3 case4
+这几个步骤，f->pc从0递增到4,函数依次执行这几个步骤，最后在case4步骤时返回
 */
