f903113d76d6c763d4882a83746588e3829e855e
123kangning <3390142252@qq.com>
2025-01-16 00:40:43 +0800

    加入kafka生产者性能测试

--- go/main/main.go
+++ go/main/main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"strings"
+	"time"
 )
 
 func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	fmt.Println(time.Now().Format(time.DateTime))
+	<-ctx.Done()
+	fmt.Println(time.Now().Format(time.DateTime))
+
 }
 func choose(a, b string) string {
 	if len(a) > len(b) {
